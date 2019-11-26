1a3899bd9697f0899fdf7aa7e1783fb19f211dfa
User17745 <AbhishekAggarwal1995@gmail.com>
2019-11-26 22:24:18 +0530

    Moved JS code from Sidebar PHP file to scripts.js

--- assets/scripts.js
+++ assets/scripts.js
@@ -38,23 +38,113 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 
 
+  //----------------------------------------------Sidebar Tab Switcher----------------------------------------------//
 
+    // Assigns onclick event listener to all the menu items.
+    const menuItems = document.getElementsByClassName("sidebar-menu-item");
+    for(let menuItem of menuItems)
+        menuItem.addEventListener("click", getNewTabName);
 
-  //User Dashboard Sidebar Tab Switcher
-  // const menuItems = document.getElementsByClassName("sidebar-menu-item");
-  // for(let menuItem of menuItems)
-  //     menuItem.addEventListener("click", changeTab);
+    // Finds the nearest parent node with a specific class for a child.
+    function getParentNodeWithClass(node, cls){
+        while ((node = node.parentNode) && !node.classList.contains(cls));
+          return node;
+    }
 
-  // function changeTab(e){
-  //     let tabName = document.getElementsByClassName("sidebar-menu-item-text")[1].innerHTML.trim();
-  //     let params = "tabName=" + tabName;
-  //       console.log(params);
-  //     let xhr = new XMLHttpRequest();
-  //     xhr.open("POST", "./user/dashboard.php", true);
-  //     xhr.setRequestHeader('content-type', 'Application/x-www-form-urlencoded');
-  //     xhr.onload = function() {
-  //       if(this.status == 200)
-  //         console.log("AJAX Success");
-  //     }
-  //     xhr.send(params);
-  // }
+    // Finds out on which tab has the user clicked on.
+    function getNewTabName(e){
+        //Note: The terms "node" and "element" have been used interchangeably in this section of comment eventhough both terms have slight differences.
+        // This "node" variable is the exact element which is clicked upon by the user.
+        let node = e.target;
+
+        /** 
+         * If the "node" is the element which contains the text that we need(Name of the tab), 
+         * we simply parse the text, trim it and call changeTab() in order for it to do it's thing. 
+        */
+        if(node.classList.contains("sidebar-menu-item-text"))
+            changeTab(node.innerHTML.trim());
+
+        /** 
+         * Else, if "node" is the immediate parent element that contains the text and the icon element, 
+         * we parse through all of the child nodes, find the node that contains our text, trim it and call changeTab(). 
+        */
+        else if(node.classList.contains("sidebar-menu-item")){
+            for(let i = 0; i < node.childNodes.length; i++)
+                if(node.childNodes[i].nodeName != "#text")
+                    if(node.childNodes[i].classList.contains("sidebar-menu-item-text"))
+                        changeTab(node.childNodes[i].innerHTML.trim());
+        }
+        /** 
+         * Otherwise, if "node" contains any other element beside the above two, 
+         * we parse through all of the parent nodes, find the immediate node that contains the text and the icon element
+         * and follow the same remaining procedure as the last case.
+        */
+        else {
+            node = getParentNodeWithClass(node, "sidebar-menu-item");
+            for(let i = 0; i < node.childNodes.length; i++)
+                if(node.childNodes[i].nodeName != "#text")
+                    if(node.childNodes[i].classList.contains("sidebar-menu-item-text"))
+                        changeTab(node.childNodes[i].innerHTML.trim());
+        }
+        
+        
+        // Highlighting the new tab with "active" class.
+        for(menuItem of menuItems)
+            menuItem.classList.remove('active');
+        if(node.classList.contains('sidebar-menu-item'))
+            node.classList.add('active');
+        else
+            node = getParentNodeWithClass(node, 'sidebar-menu-item')
+            node.classList.add('active');
+    }
+
+    // Finds out the actual PHP file name for the requested tab and loads it using AJAX and jQuery 
+    function changeTab(newTabName) {
+        // Enabling page load animation under the navigation bar
+        document.getElementById("loading-page").style.display = "block";
+        
+        let fileName;
+        
+        switch(newTabName) {
+            case 'Exams': fileName = 'exams.php';
+            break;
+            case 'My Tests': fileName = 'tests.php';
+            break;
+            //case 'Stats': fileName = 'stats.php';
+            case 'Stats': fileName = 'exam-pack.php';
+            break;
+            case 'News': fileName = 'news.php';
+            break;
+            default: fileName = 'exams.php';
+        }
+
+        // Sending fileName to dashboard to update in session.
+        $(document).ready(function() {
+            $.ajax({
+                type: "POST",
+                url: './user/dashboard.php',
+                data: {dash_page: fileName},
+                success: function(data) {
+                        
+                    }
+                });
+        });
+
+        //jQuery for AJAX to load the required PHP file
+        $(document).ready(function() {
+            $.ajax({
+                type: "POST",
+                url: './user/partials/' + fileName,
+                data: {session: '<?php echo session_id(); ?>'},
+                success: function(data) {
+                        // Loading the partial into the page.
+                        $('#user-dashboard-partial').html(data)
+                        // Hiding the page loading animation under the navigation bar
+                        $('#loading-page').hide();
+                    },
+                dataType: 'html'
+                });
+        });
+    }
+
+    //----------------------------------------------Sidebar Tab Switcher----------------------------------------------//
