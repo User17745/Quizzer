b6ec264769849cbfd27d66f3c0c504d285f66051
User17745 <AbhishekAggarwal1995@gmail.com>
2019-11-26 22:25:37 +0530

    Edited site title

--- inc/header.php
+++ inc/header.php
@@ -9,7 +9,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>PHP Core</title>
+    <title>Quizzer</title>
 
     <!-- Bulma Minified CSS CDN -->
     <link href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.5/css/bulma.min.css" rel="stylesheet" type="text/css">
